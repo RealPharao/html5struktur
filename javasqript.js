d6535eddf532a568b35f4694e9b7b0131f2b40b3
RealPharao <patrickpharao@gmail.com>
2023-08-24 11:03:27 +0200

    console.log

---
+++ javasqript.js
@@ -0,0 +1,27 @@
+// let h1 = document.createElement("h1");
+// let title = document.createTextNode("Nu står der Penis...!");
+// h1.appendChild(title);
+// console.log("nu står der noget andet");
+
+// let h1 = document.querySelector("h1");
+// let text = document.createTextNode("nu står der det her");
+// h1.appendChild(text);
+// console.log("");
+
+let h1 = document.querySelector("h1");
+console.log(h1.textContent);
+
+let h2 = document.querySelector("h2");
+console.log(h2.textContent);
+
+let h3one = document.querySelector("h3");
+console.log(h3one.textContent);
+
+let h3two = document.querySelector("article:last-child h3");
+console.log(h3two.textContent);
+
+let footer = document.querySelector("footer");
+console.log(footer.textContent);
+
+// let h1 = document.querySelector("h1");
+// console.log(h1.textContent);
