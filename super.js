92eeff0130b449a37f4efe45f4222ea101417a83
RealPharao <patrickpharao@gmail.com>
2023-08-25 12:39:56 +0200

    Superhelt

---
+++ super.js
@@ -0,0 +1,16 @@
+{
+  powers: ["Rest", "Beer", "Watching television"];
+  realName: "Los Pedro ";
+  alias: "Double p";
+  origin: "none";
+  weaknesses: ["programming", "paying attention"];
+  active: true;
+  isEvil: false;
+  height: 185;
+  birtday: {
+    day: 24;
+    month: 11;
+    year: 1901;
+  }
+  image: "https://images.pexels.com/photos/3687770/pexels-photo-3687770.jpeg?auto=compress&cs=tinysrgb&w=600";
+}
