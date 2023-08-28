5fa4611795f13dd9f42a2198d6588c919ac88e8d
RealPharao <patrickpharao@gmail.com>
2023-08-28 11:25:33 +0200

    opdatering

--- super.js
+++ super.js
@@ -1,3 +1,35 @@
+// fetch("https://kea-alt-del.dk/t7/superheroes/")
+//   .then((res) => res.json())
+//   .then(showHeroes);
+
+// function showHeroes(heros) {
+//   heros.forEach(showHero);
+// }
+
+// function showHero(heros) {
+//   console.log(heros);
+
+//   const template = document.querySelector("template").content;
+//   const copy = template.cloneNode(true);
+
+//   copy.querySelector(".power").textContent = `Power: ${heros.powers}`;
+//   copy.querySelector(".realName").textContent = `realName: ${heros.realName}`;
+//   copy.querySelector(".alias").textContent = `Alias: ${heros.alias}`;
+//   copy.querySelector(".origin").textContent = `Origin: ${heros.origin}`;
+//   copy.querySelector(
+//     ".weaknesses"
+//   ).textContent = `Weaknesses: ${heros.weaknesses}`;
+//   copy.querySelector(".active").textContent = `Active: ${heros.active}`;
+//   copy.querySelector(".isEvil").textContent = `isEvil: ${heros.isEvil}`;
+//   copy.querySelector(".height").textContent = `Height: ${heros.height}`;
+//   copy.querySelector(
+//     ".birthday"
+//   ).textContent = `Dag:${heros.birthday.day}  Måned:${heros.birthday.month}  År:${heros.birthday.year}`;
+//   copy.querySelector("img").src = heros.image;
+//   const parent = document.querySelector("main");
+//   parent.appendChild(copy);
+// }
+
 fetch("https://kea-alt-del.dk/t7/superheroes/")
   .then((res) => res.json())
   .then(showHeroes);
@@ -12,45 +44,19 @@ function showHero(heros) {
   const template = document.querySelector("template").content;
   const copy = template.cloneNode(true);
 
-  copy.querySelector(".power").textContent = heros.powers;
-  copy.querySelector(".realName").textContent = heros.realName;
-  copy.querySelector(".alias").textContent = heros.alias;
-  copy.querySelector(".origin").textContent = heros.origin;
-  copy.querySelector(".weaknesses").textContent = heros.weaknesses;
-  copy.querySelector(".active").textContent = heros.active;
-  copy.querySelector(".isEvil").textContent = heros.isEvil;
-  copy.querySelector(".height").textContent = heros.height;
-  copy.querySelector(".birthday").textContent =
-    heros.birthday.day + "/" + heros.birthday.month;
-  copy.querySelector("img").src = heros.image;
-  const parent = document.querySelector("body");
-  parent.appendChild(copy);
-}
-
-fetch("https://kea-alt-del.dk/t7/superheroes/")
-  .then((res) => res.json())
-  .then(showHeroes);
-
-function showHeroes(heros) {
-  heros.forEach(showHero);
-}
-
-function showHero(heros) {
-  console.log(heros);
-
-  const template = document.querySelector("template").content;
-  const copy = template.cloneNode(true);
-
-  copy.querySelector(".power").textContent = heros.powers;
-  copy.querySelector(".realName").textContent = heros.realName;
-  copy.querySelector(".alias").textContent = heros.alias;
-  copy.querySelector(".origin").textContent = heros.origin;
-  copy.querySelector(".weaknesses").textContent = heros.weaknesses;
-  copy.querySelector(".active").textContent = heros.active;
-  copy.querySelector(".isEvil").textContent = heros.isEvil;
-  copy.querySelector(".height").textContent = heros.height;
-  copy.querySelector(".birthday").textContent =
-    heros.birthday.day + "/" + heros.birthday.month;
+  copy.querySelector(".power").textContent = `Power: ${heros.powers}`;
+  copy.querySelector(".realName").textContent = `realName: ${heros.realName}`;
+  copy.querySelector(".alias").textContent = `Alias: ${heros.alias}`;
+  copy.querySelector(".origin").textContent = `Origin: ${heros.origin}`;
+  copy.querySelector(
+    ".weaknesses"
+  ).textContent = `Weaknesses: ${heros.weaknesses}`;
+  copy.querySelector(".active").textContent = `Active: ${heros.active}`;
+  copy.querySelector(".isEvil").textContent = `isEvil: ${heros.isEvil}`;
+  copy.querySelector(".height").textContent = `Height: ${heros.height}`;
+  copy.querySelector(
+    ".birthday"
+  ).textContent = `Dag:${heros.birthday.day}  Måned:${heros.birthday.month}  År:${heros.birthday.year}`;
   copy.querySelector("img").src = heros.image;
   const parent = document.querySelector("main");
   parent.appendChild(copy);
